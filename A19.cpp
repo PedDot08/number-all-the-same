e5162646a4325b598804b7535ed7ae88c927841f
Chonlaphat <petchtv.am@gmail.com>
2026-03-17 21:18:34 +0700

    update code number all same

--- A19.cpp
+++ A19.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
+    
+    int number1, number2, number3;
+    cin >> number1;
+    cin >> number2;
+    cin >> number3;
+
+    if (number1 == number2 && number2 == number3) {
+        cout << "all the same" << endl;
+    } else if (number1 != number2 && number2 != number3 && number3 != number1){
+        cout << "all different" << endl;
+    } else {
+        cout << "neither" << endl;
+    }
+
     return 0;
 }
